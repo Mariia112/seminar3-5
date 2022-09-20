c1be88b98777cdf6b18b5f9b4cb32676fb13c2eb
Mariia <martush1992@gmail.com>
2022-09-20 15:00:10 +0300

    Finish

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,28 +5,28 @@
 Console.WriteLine("Ввести размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] threeDigitNumbers = new int[size];
-FillArrayRandomNumbers(threeDigitNumbers);
+ArrayRandomNumbers(threeDigitNumbers);
 Console.WriteLine("Массив: ");
 PrintArray(threeDigitNumbers);
 int count = 0;
 
 for (int z = 0; z < threeDigitNumbers.Length; z++)
-if (threeDigitNumbers[z] % 2 == 0)
-count++;
+    if (threeDigitNumbers[z] % 2 == 0)
+        count++;
 
 Console.WriteLine($"всего {threeDigitNumbers.Length} чисел, {count} из них чётные");
 
-void FillArrayRandomNumbers(int[] threeDigitNumbers)
+void ArrayRandomNumbers(int[] threeDigitNumbers)
 {
-    for(int i = 0; i < threeDigitNumbers.Length; i++)
+    for (int i = 0; i < threeDigitNumbers.Length; i++)
     {
-        threeDigitNumbers[i] = new Random().Next(100,1000);
+        threeDigitNumbers[i] = new Random().Next(100, 1000);
     }
 }
 void PrintArray(int[] threeDigitNumbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < threeDigitNumbers.Length; i++)
+    for (int i = 0; i < threeDigitNumbers.Length; i++)
     {
         Console.Write(threeDigitNumbers[i] + " ");
     }
